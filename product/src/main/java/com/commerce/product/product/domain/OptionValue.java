079eb5abd4b5ac1a70d836f589cac3a08244ae69
Myungkyulee <skdlaudrb@naver.com>
2024-08-31 18:19:34 +0900

    fix: Entity 클래스 @OneToMany 단방향으로 설계변경
    상위 클래스에서 관리할 수 있도록 하여 비즈니스적으로 참조할 필요가 없는 객체의 참조를 끊어내고 부모에서 연관관계를 관리할 수 있도록 변경

--- product/src/main/java/com/commerce/product/product/domain/OptionValue.java
+++ product/src/main/java/com/commerce/product/product/domain/OptionValue.java
@@ -1,11 +1,9 @@
 package com.commerce.product.product.domain;
 
+
 import com.commerce.product.product.dto.CreateOptionValue;
 import jakarta.persistence.*;
 import lombok.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Setter
@@ -19,20 +17,7 @@ public class OptionValue {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long optionValueId;
-
     private String optionValue;
-
-/*    @Builder.Default
-    @OneToMany
-    private List<OptionCombinationValue> optionCombinationValues = new ArrayList<>();*/
-
-    @ManyToOne
-    @JoinColumn(name = "option_id")
-    private Option option;
-
-    public boolean isOptionValue(String value) {
-        return optionValue.equals(value);
-    }
 
     public static OptionValue of(CreateOptionValue.Request optionValueDto) {
         return OptionValue.builder()
